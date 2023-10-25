feeaf7614f0333273db205a9777673c9bcab59a3
devseryun <devseryun@pinotlab.com>
2023-10-25 17:48:49 +0900

    Docu page 수정

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+import Image from "next/image";
 import React from "react";
-
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
 const DocumentsPage = () => {
-  return <div>DocumentsPage page</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Rotion
+      </h2>
+      <Button>
+        <PlusCircleIcon className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
