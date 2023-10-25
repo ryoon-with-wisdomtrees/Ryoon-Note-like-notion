7b305936864e7931eac481b2843bd90282f92ad0
devseryun <devseryun@pinotlab.com>
2023-10-25 20:51:27 +0900

    왼쪽 사이드바 +new page 펑션 적용

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,16 +1,24 @@
 import { cn } from "@/lib/utils";
-import { ChevronLeftIcon, ChevronsLeft, MenuIcon } from "lucide-react";
+import {
+  ChevronLeftIcon,
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./UserItem";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import Item from "./Item";
+import { toast } from "sonner";
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px");
   const documents = useQuery(api.documents.getNote); //realtime으로 갖고옴. watching all of the changes
+  const create = useMutation(api.documents.create);
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null); //타입이 div, default va은 null로
@@ -88,6 +96,14 @@ const Navigation = () => {
     }
   };
 
+  const handleCreateNotion = () => {
+    const promise = create({ title: "Untitled" });
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note,",
+    });
+  };
   return (
     <>
       <aside
@@ -110,6 +126,11 @@ const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item
+            onClick={handleCreateNotion}
+            label="New page"
+            icon={PlusCircle}
+          />
         </div>
         <div className="mt-4">
           {documents?.map((doc) => (
