757d10824082546e382ba5b735f0212ce1c72e1f
devseryun <devseryun@pinotlab.com>
2023-10-28 20:13:53 +0900

    use-setting hooks 작성

---
+++ components/modals/SettingModal.tsx
@@ -0,0 +1,6 @@
+"use client";
+const SettingModal = () => {
+  return <div>SettingModal</div>;
+};
+
+export default SettingModal;
--- hooks/use-settings.tsx
+++ hooks/use-settings.tsx
@@ -1,1 +1,14 @@
 //global store by using zustand to track whether we have clicked on this or not.
+import { create } from "zustand";
+
+interface SettingStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export const useSetting = create<SettingStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
